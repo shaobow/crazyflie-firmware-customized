4408203bbf0e608afa628a39b257ea379ef4a271
jatajatat <jata7777@icloud.com>
2022-12-05 11:18:54 -0500

    full states K w/ gain scheduling

--- src/modules/src/controller_lqr.c
+++ src/modules/src/controller_lqr.c
@@ -12,21 +12,22 @@
 #include "controller_lqr.h"
 #include "physicalConstants.h"
 
+const static float g = 9.81;
+
 // Logging variables
 static float cmd_thrust;
 static float cmd_roll;
 static float cmd_pitch;
 static float cmd_yaw;
 
-// static float err_x;
-// static float err_y;
-// static float err_z;
+static float err_x;
+static float err_y;
+static float err_z;
 static float err_roll;
 static float err_pitch;
 static float err_yaw;
 
-// tune control thrust
-static float ctrl_thrust = 0.0;
+static int cmd_tick;
 
 # define NUM_STATE 12
 # define NUM_CTRL 4
@@ -62,6 +63,9 @@ static float K_dlqr_10_10[NUM_CTRL][NUM_STATE] = {
 auto *K_dlqr;
 K_dlqr = &K_dlqr_0_0;
 
+float const deg2millirad = (float) M_PI / 180.0f;
+const static float cf_weight = CF_MASS * g;
+
 void controllerLqrInit(void)
 {
 }
@@ -77,29 +81,35 @@ void controllerLqr(control_t *control, setpoint_t *setpoint,
                                          const uint32_t tick)
 {
   control->controlMode = controlModeForceTorque;
-  // control->controlMode = controlModeLegacy;
 
   if (!RATE_DO_EXECUTE(ATTITUDE_RATE, tick)) {
+    cmd_tick = 0;
     return;
   }
 
-  // gyro unit: rad/sec
-  float const deg2millirad = (float)M_PI / 180.0f;
   // using sensor info for state estimation of dotRoll, dotPitch, and dotYaw
-  float state_rateRoll = sensors->gyro.x * deg2millirad;
+  float state_rateRoll = sensors->gyro.x * deg2millirad; // gyro unit: rad/sec
   float state_ratePitch = -sensors->gyro.y * deg2millirad;
   float state_rateYaw = sensors->gyro.z * deg2millirad;
 
-  float u[NUM_CTRL];
+  float u[4];
 
-  float e_r = (setpoint->attitude.roll - state->attitude.roll) * deg2millirad; // unit: deg
-  float e_p = (setpoint->attitude.pitch - state->attitude.pitch) * deg2millirad;
-  float e_y = (setpoint->attitude.yaw - state->attitude.yaw) * deg2millirad;
+  float e1 = setpoint->position.x - state->position.x;
+  float e2 = setpoint->position.y - state->position.y;
+  float e3 = setpoint->position.z - state->position.z;
 
-  float e_rd = setpoint->attitudeRate.roll * deg2millirad - state_rateRoll; // setpoint->attitudeRate unit: deg/sec
-  float e_pd = setpoint->attitudeRate.pitch * deg2millirad - state_ratePitch;
-  float e_yd = setpoint->attitudeRate.yaw * deg2millirad - state_rateYaw;
-  float error[NUM_STATE] = {e_r, e_p, e_y, e_rd, e_pd, e_yd};
+  float e4 = (setpoint->attitude.roll - state->attitude.roll) * deg2millirad; // unit: deg
+  float e5 = (setpoint->attitude.pitch - state->attitude.pitch) * deg2millirad;
+  float e6 = (setpoint->attitude.yaw - state->attitude.yaw) * deg2millirad;
+
+  float e7 = setpoint->velocity.x - state->velocity.x;
+  float e8 = setpoint->velocity.y - state->velocity.y;
+  float e9 = setpoint->velocity.z - state->velocity.z;
+
+  float e10 = setpoint->attitudeRate.roll * deg2millirad - state_rateRoll; // setpoint->attitudeRate unit: deg/sec
+  float e11 = setpoint->attitudeRate.pitch * deg2millirad - state_ratePitch;
+  float e12 = setpoint->attitudeRate.yaw * deg2millirad - state_rateYaw;
+  float error[12] = {e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12};
   int i = 0, j = 0; 
   float res = 0;
 
@@ -130,43 +140,43 @@ void controllerLqr(control_t *control, setpoint_t *setpoint,
   }
 
   /* feedback */
-  control->thrustSi = CF_MASS * 10.0f;
-  control->torqueX = u[0];
-  control->torqueY = u[1];
-  control->torqueZ = u[2];
+  control->thrustSi = u[0] + cf_weight;
+  control->torqueX = u[1];
+  control->torqueY = u[2];
+  control->torqueZ = u[3];
 
   cmd_thrust = control->thrustSi;
   cmd_roll = control->torqueX;
   cmd_pitch = control->torqueY;
   cmd_yaw = control->torqueZ;
 
-  err_roll = e_r;
-  err_pitch = e_p;
-  err_yaw = e_y;
+  err_x = e1;
+  err_y = e2;
+  err_z = e3;
+  err_roll = e4;
+  err_pitch = e5;
+  err_yaw = e6;
+
+  cmd_tick = 1;
 }
-
-/**
- * Tunning variables for the full state Controller
- */
-PARAM_GROUP_START(ctrlMel)
-/**
- * @brief Thrust value
- */
-PARAM_ADD_CORE(PARAM_FLOAT | PARAM_PERSISTENT, ctrl_thrust, &ctrl_thrust)
-
-PARAM_GROUP_STOP(ctrlMel)
 
 /**
  * Logging variables for the command and reference signals for the
  * Mellinger controller
  */
+
 LOG_GROUP_START(ctrlMel)
 LOG_ADD(LOG_FLOAT, cmd_thrust, &cmd_thrust)
 LOG_ADD(LOG_FLOAT, cmd_roll, &cmd_roll)
 LOG_ADD(LOG_FLOAT, cmd_pitch, &cmd_pitch)
 LOG_ADD(LOG_FLOAT, cmd_yaw, &cmd_yaw)
 
+LOG_ADD(LOG_FLOAT, err_x, &err_x)
+LOG_ADD(LOG_FLOAT, err_y, &err_y)
+LOG_ADD(LOG_FLOAT, err_z, &err_z)
 LOG_ADD(LOG_FLOAT, err_roll, &err_roll)
 LOG_ADD(LOG_FLOAT, err_pitch, &err_pitch)
 LOG_ADD(LOG_FLOAT, err_yaw, &err_yaw)
+
+LOG_ADD(LOG_INT8, cmd_tick, &cmd_tick)
 LOG_GROUP_STOP(ctrlMel)
